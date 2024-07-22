f7a491a576e030baa2c77033e3e18aecbc9dce13
jean <jeancarloe01@hotmail.com>
2024-07-21 18:59:21 -0600

    adding container name to the output

--- docker-containers/outputs.tf
+++ docker-containers/outputs.tf
@@ -1,4 +1,4 @@
 output "container_id" {
   description = "Container id:"
-  value = "${docker_container.docker-container.id} at port -> ${docker_container.docker-container.ports[0].external}"
+  value = "${docker_container.docker-container.id} (${docker_container.docker-container.name}) at port -> ${docker_container.docker-container.ports[0].external}"
 }
