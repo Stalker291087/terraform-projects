772846b195e3bf627e90b79f6aa0e0bead88a2c8
jean <jeancarloe01@hotmail.com>
2024-07-22 18:41:20 -0600

    modifications on hcl config files

--- docker-containers/container.tf
+++ docker-containers/container.tf
@@ -1,13 +1,21 @@
 # Resource block to create the docker image
+resource "random_string" "random-string" {
+  length  = 4
+  special = false
+  upper   = false
+  count = 2
+}
+# Creating docker image
 resource "docker_image" "docker-image" {
   name = var.docker-image
 }
-
+# Creating docker container
 resource "docker_container" "docker-container" {
-  name     = var.container-name
-  image    = docker_image.docker-image.name
+  count = 2
+  name  = join("-", [var.container-name, random_string.random-string[count.index].result])
+  image = docker_image.docker-image.name
   ports {
     internal = var.docker-ports["host"]
-    external = var.docker-ports["container"]
+    #external = var.docker-ports["container"]
   }
 }
--- docker-containers/outputs.tf
+++ docker-containers/outputs.tf
@@ -1,4 +1,4 @@
-output "container_id" {
-  description = "Container id:"
-  value = "${docker_container.docker-container.id} (${docker_container.docker-container.name}) at port -> ${docker_container.docker-container.ports[0].external}"
+output "container-details" {
+  description = "Docker containers and external ports."
+  value = [for ports in docker_container.docker-container[*] : join(":", [ports.name], [ports.network_data[0].ip_address], [ports.ports[0]["external"]])]
 }
